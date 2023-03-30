419e6f630bbcc1d8db1da2bb910ce47002421f81
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-03-31 00:16:47 +0500

    CreateDll

---
+++ DLL/DllFioGroup/DllFioGroup/DllFioGroup.cpp
@@ -0,0 +1,22 @@
+﻿// DllFioGroup.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DllFioGroup.h"
+
+
+//// This is an example of an exported variable
+//DLLFIOGROUP_API int nDllFioGroup=0;
+//
+//// This is an example of an exported function.
+//DLLFIOGROUP_API int fnDllFioGroup(void)
+//{
+//    return 0;
+//}
+//
+//// This is the constructor of a class that has been exported.
+//CDllFioGroup::CDllFioGroup()
+//{
+//    return;
+//}
