2250e4a8614fd45b5e03948bcb6b020893f489e0
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-04-03 01:57:03 +0500

    Add functions Fio and Group

--- DLL/DllFioGroup/DllFioGroup/DllFioGroup.cpp
+++ DLL/DllFioGroup/DllFioGroup/DllFioGroup.cpp
@@ -4,7 +4,16 @@
 #include "pch.h"
 #include "framework.h"
 #include "DllFioGroup.h"
+#include <iostream>
+using namespace std;
 
+string Fio() {
+	return "Borodashkina Ekaterina Alekseevna";
+}
+
+int Group() {
+	return 581220;
+}
 
 //// This is an example of an exported variable
 //DLLFIOGROUP_API int nDllFioGroup=0;
