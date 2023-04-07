c2c72a25fc913c1222e9752088c80a70a94878f8
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-04-07 23:09:00 +0500

    Create DLL (RAD Studio) and Console Application (Visual Studio), Delete DLL (VS)

---
+++ DLL/VS_DZ_1/VS_Console/VS_Console/VS_Console.cpp
@@ -0,0 +1,33 @@
+﻿// VS_Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+
+// объявление прототипов библиотечных функций в виде указателей на функцию. Объявляются там где будут использоваться.
+typedef const char*(__stdcall* FIO)(); // typedef возвращаемый_тип (<соглашение о вызове функции (такое же как в библиотеке)> *имя_указателя) (типы_параметров);
+typedef int(__stdcall* Group)();
+int main() {
+    HMODULE h = LoadLibraryW(L"C:\\Users\\Екатерина\\Desktop\\5 курс\\2 семестр\\ИБТС\\ISTS_URFU\\DLL\\RS_DZ_1\\dll\\Win64\\Debug\\dll.dll");
+    if (h == NULL) { // проверить успешно ли подключилась библиотека
+        printf("load lib error\n");
+        system("pause");
+        exit(1);
+    }
+    FIO ptr_to_fun1 = (FIO)GetProcAddress(h, "FIO"); // указатель на функцию (вызывать как обычную функцию)
+    Group ptr_to_fun2 = (Group)GetProcAddress(h, "Group");
+
+    // вызов функции: возвращаемый_тип имя_переменной = ptr_to_fun1(параметры);
+    setlocale(LC_ALL, "rus");
+    std::cout << "Домашняя работа номер 1, DLL\n\n";
+    int group = ptr_to_fun2();
+    if (!group) printf("unable to load Group\n");
+    const char* fio = ptr_to_fun1();
+    if (!fio) printf("unable to load FIO\n");
+    std::cout << "Номер группы: " << group << "\n";
+    std::cout << "ФИО: " << fio << "\n";
+
+    FreeLibrary(h);
+    system("pause");
+    return 0;
+}
