277ddae65f1e572ef9e142ccc9f96bcf01ddd289
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-04-15 12:53:51 +0500

    Reading disk as an administrator and working out error

--- NTFS/NTFS/NTFS/NTFS.cpp
+++ NTFS/NTFS/NTFS/NTFS.cpp
@@ -9,6 +9,25 @@ int main()
 {
     BYTE dataBuffer[512];
     NTFSBootRecord* pNTFSBootRecord = (NTFSBootRecord*)dataBuffer;
+
+    HANDLE cFile = CreateFileW(
+        L"\\\\.\\C:",    // Открываем диск С
+        GENERIC_READ,   //  Запрос доступа к диску для чтения
+        FILE_SHARE_READ | FILE_SHARE_WRITE, //  Режим совместного использования
+        NULL,   //  Атрибуты безопасности - по умолчанию
+        OPEN_EXISTING,  //  Открытие диска, только если он существует
+        FILE_ATTRIBUTE_NORMAL,  //  Атрибуты по умолчанию
+        NULL  //  Не копировать атрибуты диска
+    );
+
+    // Ошибка открытия диска
+    if (cFile == INVALID_HANDLE_VALUE) {
+        printf("\nDisk read ERROR :(\n\nTry to run as an administrator\n");
+        system("pause");
+        return 0;
+    }
+    else { printf("It's okey ^_^\n"); system("pause"); return 0;
+    }
 }
 
 
