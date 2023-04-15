52320fdca43dd0ee0614d7d4214115710106b10d
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-04-15 11:42:17 +0500

    Create file NTFSDriver.h

--- NTFS/NTFS/NTFS/NTFS.cpp
+++ NTFS/NTFS/NTFS/NTFS.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
+#include <windows.h>
+#include "NTFSDriver.h"
 
 int main()
 {
-    
+    BYTE dataBuffer[512];
+    NTFSBootRecord* pNTFSBootRecord = (NTFSBootRecord*)dataBuffer;
 }
 
 
